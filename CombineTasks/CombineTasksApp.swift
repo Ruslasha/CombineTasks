83fc611fbb9cafac67402d64f23edbcf176bf4f2
Ruslan <ruslan99992222@gmail.com>
2024-05-20 18:18:45 +0300

    Initial Commit

---
+++ CombineTasks/CombineTasksApp.swift
@@ -0,0 +1,17 @@
+//
+//  CombineTasksApp.swift
+//  CombineTasks
+//
+//  Created by Руслан Абрамов on 20.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct CombineTasksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
