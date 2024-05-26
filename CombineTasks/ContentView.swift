df6526ef403d087aa945baa1e5b45f778abe36c2
Ruslan <ruslan99992222@gmail.com>
2024-05-26 16:15:47 +0300

    complete task 10

--- CombineTasks/ContentView.swift
+++ CombineTasks/ContentView.swift
@@ -6,19 +6,74 @@
 //
 
 import SwiftUI
+import Combine
 
-struct ContentView: View {
+enum ViewState<Model> {
+    case loading
+    case data(_ data: Model)
+    case error(_ error: Error)
+}
+
+struct PassThroughSubjectTimerView: View {
+
+    @StateObject private var viewModel = PassThroughSubjectTimerViewModel()
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            switch viewModel.state {
+                case .loading:
+                    Button("Старт") {
+                        viewModel.verifyState.send("00:00")
+                        viewModel.start()
+                    }
+                case .data(let time):
+                    Text(time)
+                        .font(.title)
+                        .foregroundStyle(.green)
+                case .error(let error):
+                    Text(error.localizedDescription)
+            }
         }
-        .padding()
+    }
+}
+
+final class PassThroughSubjectTimerViewModel: ObservableObject {
+    @Published var state: ViewState<String> = .loading
+
+    let verifyState = PassthroughSubject<String, Never>()
+
+    var cancellable: AnyCancellable?
+    var timerCancellable: AnyCancellable?
+
+    init() {
+        bind()
+    }
+
+    func bind() {
+        cancellable = verifyState
+            .sink(receiveValue: { [unowned self] value in
+                if !value.isEmpty {
+                    state = .data(value)
+                    start()
+                } else {
+                    state = .error(NSError(domain: "Error time", code: 101))
+                }
+            })
+    }
+
+    func start() {
+        let timeFormat = DateFormatter()
+        timeFormat.dateFormat = "00:ss"
+
+        timerCancellable = Timer
+            .publish(every: 1, on: .main, in: .common)
+            .autoconnect()
+            .sink(receiveValue: { [unowned self] datum in
+                verifyState.send(timeFormat.string(from: datum))
+            })
     }
 }
 
 #Preview {
-    ContentView()
+    PassThroughSubjectTimerView()
 }
